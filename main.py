93c54cb5dc44ab6835e3052056c8698ca5e6a4dd
Michael Brewster <brew@withdatalove.com>
2024-01-25 16:42:22 +0900

    initial pass

---
+++ main.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+from source_mobile_commons.run import run
+
+if __name__ == "__main__":
+    run()
---
+++ setup.py
@@ -0,0 +1,36 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+
+from setuptools import find_packages, setup
+
+MAIN_REQUIREMENTS = [
+    "airbyte-cdk~=0.2",
+    "xmltodict~=0.13.0"
+]
+
+TEST_REQUIREMENTS = [
+    "requests-mock~=1.9.3",
+    "pytest~=6.2",
+    "pytest-mock~=3.6.1",
+    "connector-acceptance-test",
+]
+
+setup(
+    name="source_mobile_commons",
+    description="Source implementation for Mobile Commons.",
+    author="Airbyte",
+    author_email="contact@airbyte.io",
+    packages=find_packages(),
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json", "*.yaml", "schemas/*.json", "schemas/shared/*.json"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
+    entry_points={
+        "console_scripts": [
+            "source-mobile-commons=source_mobile_commons.run:run",
+        ],
+    },
+)
