e1477887d2af39e9ef715979de9becc61bb80308
Michael Brewster <brew@withdatalove.com>
2024-02-13 16:51:44 +0700

    working version

--- source_mobile_commons/source.py
+++ source_mobile_commons/source.py
@@ -287,32 +287,32 @@ class IncomingMessages(MobileCommonsStream):
 
 
 # Basic incremental stream
-class IncrementalMobileCommonsStream(MobileCommonsStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
+# class IncrementalMobileCommonsStream(MobileCommonsStream, ABC):
+#     """
+#     TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
+#          if you do not need to implement incremental sync for any streams, remove this class.
+#     """
 
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
+#     # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
+#     state_checkpoint_interval = None
 
-    @property
-    def cursor_field(self) -> str:
-        """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
+#     @property
+#     def cursor_field(self) -> str:
+#         """
+#         TODO
+#         Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
+#         usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
 
-        :return str: The name of the cursor field.
-        """
-        return []
+#         :return str: The name of the cursor field.
+#         """
+#         return []
 
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        return {}
+#     def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+#         """
+#         Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
+#         the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
+#         """
+#         return {}
 
 class Keywords(MobileCommonsStream):
     """
@@ -421,7 +421,7 @@ class Profiles(MobileCommonsStream, IncrementalMixin):
             }
             dates.append(from_to_dates)
             start_date += timedelta(days=1)
-        return dates[0:10]
+        return dates
     
     def stream_slices(
         self,
@@ -458,12 +458,12 @@ class Profiles(MobileCommonsStream, IncrementalMixin):
     def read_records(self, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
         for record in super().read_records(*args, **kwargs):
             print(f"self._cursor_value: {self._cursor_value}")
+            latest_record_date = datetime.strptime(record[self.cursor_field], '%Y-%m-%d %H:%M:%S %Z')
+            
             if self._cursor_value:
-                latest_record_date = datetime.strptime(record[self.cursor_field], '%Y-%m-%d %H:%M:%S %Z')
-                print(f"This:{max(self._cursor_value, latest_record_date)}")
-                print(f"Before:{self._cursor_value}")
                 self._cursor_value = max(self._cursor_value, latest_record_date)
-                print(f"After:{self._cursor_value}")
+            else:
+                self._cursor_value = latest_record_date
             yield record
 
 
